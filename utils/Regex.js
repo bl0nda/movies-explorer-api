0be4bd37eb7efbd059d2f10094452c1ce6eb80a0
DianaK <v.sesenkov@yandex.ru>
2023-09-05 11:41:26 +0300

    правка regex

--- utils/Regex.js
+++ utils/Regex.js
@@ -1,6 +1,5 @@
-// eslint-disable-next-line no-useless-escape
-const URLRegex = /^https?:\/\/(www\.)?[a-z0-9\-._~:\/?#[\]@!$&'()*+,;=]+\.[a-z0-9\-._~:\/?#[\]@!$&'()*+,;=]+#?/;
-const EmailRegex = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+/;
+const URLRegex = /^https?:\/\/(www\.)?[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]+\.[a-z0-9\-._~:/?#[\]@!$&'()*+,;=]+#?/;
+const EmailRegex = /^[a-zA-Z0-9]([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+){1,}\.([a-zA-Z]+)$/;
 
 module.exports = {
   URLRegex,
